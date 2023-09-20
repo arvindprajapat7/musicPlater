821bc10574c9b217fe8deaa0ea155a7c23136d2a
arvindprajapat7 <145344574+arvindprajapat7@users.noreply.github.com>
2023-09-20 14:18:19 +0000

    conversion of DataTypes

---
+++ conversion-of-dataTypes.js
@@ -0,0 +1,80 @@
+// let value = null; //typeof  = 0
+// let value = undefined; //typeof  = NaN
+// let value = "abc" // typeof  = NaN
+// let value = "55"; //typeof  = string
+
+// let conType = Number(value);
+// console.log( typeof conType);
+
+
+// question 01 
+
+// let a = "50";
+// let convertVal =  Number(a);
+// console.log(typeof convertVal) // number
+
+// question 02 
+// let val = "50";
+// Number(val);
+// console.log(typeof val) // string
+
+// question 03
+
+// let val = null;
+// Number(val);
+// console.log(val); // object
+
+// question 04 
+
+// let val = null;
+// let convertVal = Number(val);
+// console.log(typeof convertVal); // number
+
+// question 05
+
+// let val = undefined;
+// Number(val);
+// console.log(typeof val); // undefined
+
+// question 06 
+// let val = undefined;
+// let convertVal = Number(val);
+// console.log(typeof convertVal); // number
+
+// question 07
+// let val = "50";
+// let convertVal = String(val);
+// console.log(typeof convertVal); // string
+
+// question 08
+// let  val = undefined;
+// let convertVal = String(val);
+// console.log(typeof convertVal); // string
+
+// question 09
+// let val = null;
+// String(val);
+// console.log(typeof val) // object
+
+
+// question 10
+// let val = null;
+// let convertVal = String(val);
+// console.log(typeof convertVal); // string
+
+
+
+// play with boolean 
+
+// #1
+let val = "arvind" ; 
+let convertVal = Boolean(val);
+//console.log(convertVal); // 1 =  true, 0 = false, "" = false, "arvind" = true,
+
+// #2
+
+
+
+
+
+
