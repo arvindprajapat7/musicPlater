c6524c9ec8c779e56aa30d4958ac4d1ef7aae13c
arvindprajapat7 <145344574+arvindprajapat7@users.noreply.github.com>
2023-09-21 09:45:10 +0000

    comparasion operator

---
+++ 05_comparasion.js
@@ -0,0 +1,10 @@
+// console.log("3" > 1); // true
+// console.log(null == 0); // false
+// console.log(null>0) ; //false
+// console.log(null>=0) ; // true
+
+console.log(undefined==0); // false
+console.log(undefined > 0); // false
+console.log(undefined < 0); // false
+console.log(undefined == null); // true
+console.log(undefined == undefined); // true
